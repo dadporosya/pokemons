77d5a702571cf172e7e52cd58bb1cfb954dfde45
dadporosya <ilja.amogus@gmail.com>
2025-09-14 11:56:25 +0300

    battle

--- logic.py
+++ logic.py
@@ -1,10 +1,29 @@
 from random import randint
 import requests
 
+class Move():
+    def __init__(self, name, damage, entry, accuracy, pokemon_attack):
+        self.name = name
+        self.damage = damage
+        self.entry = entry
+        self.accuracy = accuracy
+        self.pokemon_attack = pokemon_attack
+
+    def use(self):
+        if randint(1, 100) <= self.accuracy:
+            damage = self.damage * self.pokemon_attack / 100
+        else:
+            damage = 0
+        return {
+            "entry": self.entry,
+            "damage": damage
+        }
+
+
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer, front=True, shiny=False):
+    def __init__(self, pokemon_trainer, front=True, shiny=False, level=1):
 
         self.pokemon_trainer = pokemon_trainer
 
@@ -18,19 +37,63 @@ class Pokemon:
         self.possible_orientations = ["back", "front"]
         self.possible_shiny = ["default", "shiny"]
 
+        self.start_attack, self.start_hp, self.start_defense = self.get_stats()
+        self.hp = self.start_hp
+        self.attack = self.start_attack
+        self.defense = self.start_defense
+
 
         self.img = self.get_img()
         self.name = self.get_name()
         self.ability = self.get_ability()
+        self.abilities = []
+        for ability in self.ability:
+            self.abilities.append(ability["ability"]["name"])
+
+        self.moves_raw = self.get_moves()
+        self.moves = []
+        for move in self.moves_raw.values():
+            self.moves.append(move)
+
+        if level <= 0:
+            level = 1
+        self.level = level
+        self.exp = 0
+        self.get_new_lvl()
+
+
+
 
         Pokemon.pokemons[pokemon_trainer] = self
 
+    def get_new_lvl(self):
+        while self.exp >= (self.level+1)*10:
+            self.level += 1
+            self.exp -= self.level*10
+        self.attack = int(self.start_attack*(1 + self.level*0.2))
+        self.hp = int(self.start_hp * (1 + self.level * 0.2))
+        self.defense = int(self.start_defense * (1 + self.level * 0.2))
+
+
+
     # Метод для получения картинки покемона через API
+    def get_stats(self):
+        response = requests.get(self.pokemon_url)
+        if response.status_code == 200:
+            data = response.json()
+            attack = data["stats"][1]["base_stat"]
+            hp = data["stats"][0]["base_stat"]
+            defense = data["stats"][2]["base_stat"]
+            return attack,hp,defense
+        else:
+            return 0,0,0
+
     def get_img(self):
         response = requests.get(f"https://pokeapi.co/api/v2/pokemon-form/{self.pokemon_number}")
         if response.status_code == 200:
             data = response.json()
-            img_front_url = data['sprites'][f'{self.possible_orientations[self.front]}_{self.possible_shiny[self.shiny]}']
+            img_front_url = data['sprites'][
+                f'{self.possible_orientations[self.front]}_{self.possible_shiny[self.shiny]}']
             return img_front_url
         else:
             return "https://demofree.sirv.com/nope-not-here.jpg"
@@ -68,20 +131,61 @@ class Pokemon:
                 "slot": None
             }]
 
+    def get_moves(self):
+        response = requests.get(self.pokemon_url)
+        moves = {"pass": Move("pass", 0, "You've passed", 0, 0)}
+        if response.status_code == 200:
+            data = response.json()
+            raw_moves = data["moves"]
+            for move in raw_moves:
+                move = move["move"]
+                move_url = move["url"]
+                name = move["name"]
+                move_response = requests.get(move_url)
+                if move_response.status_code == 200:
+                    move_data = move_response.json()
+                    damage = move_data["power"]
+                    if not damage or damage == "null":
+                        damage = 0
+
+                    accuracy = move_data["accuracy"]
+                    if not accuracy or accuracy == "null":
+                        accuracy = 0
+                    try:
+                        entry = move_data["effect_entries"][0]["effect"]
+                    except:
+                        entry = "Nothing happens..."
+                    if not accuracy or accuracy == "null":
+                        entry = "Nothing happens..."
+                else:
+                    damage = 0
+                    accuracy = 0
+                    entry = "Nothing happens..."
+
+                moves[name] = Move(name, damage, entry,accuracy, self.attack)
+
+        return moves
+
     def show_abilities(self):
-        abilities = []
-        for ability in self.ability:
-            abilities.append(ability["ability"]["name"])
-        return abilities
+        return self.abilities
+
+    def show_moves(self):
+        return self.moves
 
 
 
     # Метод класса для получения информации
     def info(self):
-        message = (f"Имя твоего покемона: {self.name}\n"
+        message = (f"Имя покемона: {self.name}\n"
+                   f"Статы твоего покемона:\n"
+                   f"Атака: {self.attack}\n"
+                   f"Защита: {self.defense}\n"
+                   f"Здоровье: {self.hp}\n"
+                   f"Уровень: {self.level}\n"
+                   f"Опыт: {self.exp}\n"
                    f"Способности твоего покемона: ")
-        for ab in self.show_abilities():
-            message += ab + ", "
+        for ab in self.moves:
+            message += ab.name + ", "
         message = message[:-2]
             #f"Способности твоего покемона: {[print(ab, end="") for ab in self.show_abilities()]}"
         return message
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import telebot 
+import telebot
 from config import token
 
 from logic import Pokemon
 
-bot = telebot.TeleBot(token) 
+from random import choice, randint
 
+bot = telebot.TeleBot(token)
+
+battle = False
+enemy = None
+
+
+# your_turn = True
+
+class Battle:
+    battle = False
 
 
 @bot.message_handler(commands=['go'])
@@ -17,19 +27,30 @@ def go(message):
         bot.reply_to(message, "Ты уже создал себе покемона")
 
 
+@bot.message_handler(commands=['info'])
+def info(message):
+    if message.from_user.id in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[message.from_user.id]
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        bot.reply_to(message, "You do not have a pokemon! Use cmd /go to create new one!")
+
+
 @bot.message_handler(commands=['turn'])
-def show(message):
+def turn(message):
     if message.from_user.id in Pokemon.pokemons.keys():
         Pokemon.pokemons[message.from_user.id].turn()
         pokemon = Pokemon.pokemons[message.from_user.id]
-        bot.reply_to(message, f"Your pokemon orientation has been changed to {pokemon.possible_orientations[pokemon.front]}")
+        bot.reply_to(message,
+                     f"Your pokemon orientation has been changed to {pokemon.possible_orientations[pokemon.front]}")
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "You do not have any pokemon. Use '/go' to create one!")
 
 
 @bot.message_handler(commands=['shine'])
-def show(message):
+def shine(message):
     if message.from_user.id in Pokemon.pokemons.keys():
         Pokemon.pokemons[message.from_user.id].shine()
         pokemon = Pokemon.pokemons[message.from_user.id]
@@ -38,5 +59,97 @@ def show(message):
     else:
         bot.reply_to(message, "You do not have any pokemon. Use '/go' to create one!")
 
-bot.infinity_polling(none_stop=True)
 
+def create_btns_for_moves(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btns = []
+    for move in Pokemon.pokemons[message.from_user.id].moves:
+        btns.append(telebot.types.KeyboardButton(move.name))
+    markup.add(*btns)
+
+    bot.send_message(message.chat.id, "Choose your move", reply_markup=markup)
+
+
+@bot.message_handler(commands=['battle'])
+def battle(message):
+    battle = Battle.battle
+    if not battle:
+        Battle.battle = True
+        bot.reply_to(message, f"BATTLE stars!!!")
+        pokemon = Pokemon("ENEMY", shiny=choice([True, False]),
+                          level=Pokemon.pokemons[message.from_user.id].level + randint(-1, 1))
+        bot.send_message(message.chat.id, "YOUR ENEMY IS...")
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+
+        create_btns_for_moves(message)
+    else:
+        bot.reply_to(message, f"BATTLE has already been started!")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    user_pokemon = Pokemon.pokemons[message.from_user.id]
+
+    if message.text in user_pokemon.moves_raw.keys():
+        move = message.text
+    else:
+        move = None
+    battle = Battle.battle
+    if battle:
+        enemy = Pokemon.pokemons["ENEMY"]
+        use = user_pokemon.moves_raw[move].use()
+        dealt_damage = int(use["damage"] * (1 - enemy.defense / 100))
+        bot.reply_to(message, use["entry"])
+        bot.send_message(message.chat.id, f"You have dealt {dealt_damage} damage")
+        Pokemon.pokemons["ENEMY"].hp -= dealt_damage
+
+        if Pokemon.pokemons["ENEMY"].hp < 0:
+            Pokemon.pokemons["ENEMY"].hp = 0
+        bot.send_message(message.chat.id, f"Enemy hp: {Pokemon.pokemons["ENEMY"].hp}")
+
+        if Pokemon.pokemons["ENEMY"].hp <= 0:
+            bot.send_message(message.chat.id, f"YOU WIN!")
+            Battle.battle = False
+
+            exp = Pokemon.pokemons["ENEMY"].level * 10 + randint(0, Pokemon.pokemons["ENEMY"].level * 10)
+            old_level = user_pokemon.level
+            bot.send_message(message.chat.id, f"You have earned {exp} exp!")
+
+            Pokemon.pokemons[message.from_user.id].exp += exp
+            Pokemon.pokemons[message.from_user.id].get_new_lvl()
+
+            bot.send_message(message.chat.id, f"Your level has increased from {old_level}"
+                                              f"to {Pokemon.pokemons[message.from_user.id].level}!")
+
+            # exp and level
+        else:
+            enemy_move = choice(enemy.moves)
+            bot.send_message(message.chat.id, f"Enemy's move: {enemy_move.name}")
+            use = enemy_move.use()
+            bot.send_message(message.chat.id, use["entry"])
+            dealt_damage = int(use["damage"] * (1 - user_pokemon.defense / 100))
+            Pokemon.pokemons[message.from_user.id].hp -= dealt_damage
+
+            bot.send_message(message.chat.id, f"Enemy has dealt {dealt_damage} damage")
+
+            if Pokemon.pokemons[message.from_user.id].hp < 0:
+                Pokemon.pokemons[message.from_user.id].hp = 0
+            bot.send_message(message.chat.id, f"Your hp: {Pokemon.pokemons[message.from_user.id].hp}")
+
+            if Pokemon.pokemons[message.from_user.id].hp <= 0:
+                bot.send_message(message.chat.id, f"YOU LOSE! :(((((")
+                Pokemon.pokemons[message.from_user.id].hp = Pokemon.pokemons[message.from_user.id].start_hp
+                Battle.battle = False
+                return
+
+            create_btns_for_moves(message)
+
+    else:
+        if move:
+            bot.reply_to(message, f"{move}:\n"
+                                  f"Damage: {user_pokemon.moves_raw[move].damage}\n"
+                                  f"Accuracy: {user_pokemon.moves_raw[move].accuracy}\n")
+
+
+bot.infinity_polling(none_stop=True)
